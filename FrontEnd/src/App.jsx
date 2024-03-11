f9001fe888b327aeb5f4cef1db2afe055c70e125
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2024-03-11 14:58:06 +0200

    Prémière mise à jour

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <button>
+        test
+      </button>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas commodi soluta consequatur enim, a iusto pariatur et perferendis nobis, impedit deserunt minima aperiam autem incidunt quae aspernatur at rem aut!
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
