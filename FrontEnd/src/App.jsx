406f19ff60de81a86b808c8b7d3da5b81941d026
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2024-03-14 17:19:43 +0200

    Mise à jour du 14 mars 2024

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -8,18 +8,26 @@ function App() {
 
   return (
     <>
-    <div className=' h-80'>
-      <h1 className=' size-20 h-20 text-center text-white '>
-        
-      </h1>
-      <h2 className=' text-white size-4'>
-      Closer the university of the student
-      </h2>
-      <p className=' text-white opacity-50 text-center'>
-      Makes sure you have an account before to get started.
-      </p>
-    </div>
-    <Item_1 />
+      <div className=' text-white h-80 w-full'>
+        <div className='flex justify-between'>
+          <div>
+            <h1 className=' w-full text-left font-bold text-5xl p-2'>J</h1>
+            <h1 className=' w-full text-left font-bold text-5xl p-2'>E</h1>
+            <h1 className=' w-full text-left font-bold text-5xl p-2'>M</h1>
+
+          </div>
+          <div>
+            <button className='p-2'>Menu</button>
+          </div>
+        </div>
+        <h1 className='  w-full text-center text-3xl font-bold p-5 '>
+          Closer the university of the student
+        </h1>
+        <h2 className=' opacity-50 p-2 text-center'>
+          Makes sure you have an account before to get started.
+        </h2>
+      </div>
+      <Item_1 />
     </>
   )
 }
