0737ff7211f119f4938e85a928512fc579f00114
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2024-03-14 17:57:59 +0200

    Adding logo

--- FrontEnd/src/components/Item_1.jsx
+++ FrontEnd/src/components/Item_1.jsx
@@ -5,7 +5,10 @@ import Btn_Log from './Btn_Log'
 export default function 
 () {
   return (
-    <div className='bg-white rounded-lg h-full pb-80  w-full text-center pt-60'>
+    <div className='bg-white rounded-lg h-full pb-80  w-full text-center'>
+      <div className='p-40 text-center rounded-lg border-solid '>
+      <img className=' size-40 ' src="../src/images/profile-pic.png" alt="logo" />
+      </div>
         <Btn_Get />
         <Btn_Log />
     </div>
