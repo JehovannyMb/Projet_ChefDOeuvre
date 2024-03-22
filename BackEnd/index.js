edcfe00bb09e39911a4e81f94a4fed9e8f3001b9
Jehovanny Mbenga <mbengajehovanny@gmail.com>
2024-03-22 17:58:42 +0200

    Installation et début de postgreSQL

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const { Pool} = require('pg');
+const path = require('path')
 
+const pool = new Pool({
+    user: "admin",
+    host: "localhost",
+    database: "OO1",
+    password: "admin",
+    port: 3000
+})
+console.log("connexion à la base de données réussite")
 const app = express();
 
 const port = 3000;
 
+app.set("view engine","ejs");
+app.set("views",__dirname +"/views");
 app.get('/',(req,res)=>{
     res.send('Page principale')
+    // res.render('index');
 })
 
 app.post('/',(req,res)=>{
